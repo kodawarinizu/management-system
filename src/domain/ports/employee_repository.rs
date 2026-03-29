586211ab1707e7720048afd19d1899cbd2da23d9
kodawarinizu <tomas.serey.gonzalez@protonmail.com>
2026-03-29 14:53:38 -0300

    feat (application): add employee_repository async trait

--- src/domain/ports/employee_repository.rs
+++ src/domain/ports/employee_repository.rs
@@ -1,11 +1,13 @@
 use uuid::Uuid;
+use async_trait::async_trait;
 use crate::domain::entities::employee::Employee;
 use crate::domain::errors::DomainError;
 
+#[async_trait]
 pub trait EmployeeRepository {
-    fn save(&self, employee: Employee) -> Result<(), DomainError>;
-    fn find_by_id(&self, id: Uuid) -> Result<Option<Employee>, DomainError>;
-    fn find_by_email(&self, email: &str) -> Result<Option<Employee>, DomainError>;
-    fn update(&self, employee: Employee) -> Result<(), DomainError>;
-    fn delete(&self , id: Uuid) -> Result<(), DomainError>;
+    async fn save(&self, employee: Employee) -> Result<(), DomainError>;
+    async fn find_by_id(&self, id: Uuid) -> Result<Option<Employee>, DomainError>;
+    async fn find_by_email(&self, email: &str) -> Result<Option<Employee>, DomainError>;
+    async fn update(&self, employee: Employee) -> Result<(), DomainError>;
+    async fn delete(&self , id: Uuid) -> Result<(), DomainError>;
 }
