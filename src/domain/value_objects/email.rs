6cdb9d746e19d2efb74f38dda60755693981e5f2
kodawarinizu <tomas.serey.gonzalez@protonmail.com>
2026-03-30 13:38:06 -0300

    refactor (Email): Change email validation with regex pattern

--- src/domain/value_objects/email.rs
+++ src/domain/value_objects/email.rs
@@ -1,17 +1,19 @@
 use crate::domain::errors::DomainError;
-
+use regex::Regex;
 
 
 #[derive(Debug)]
 pub struct Email(String);
 impl Email {
     pub fn new (value: &str) -> Result<Self, DomainError> {
-        if value.contains('@') && value.len() > 5 {
-            Ok(Self(value.to_lowercase()))
+        let email_regex = Regex::new(r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$")
+        .map_err(|e| DomainError::InvalidEmail(e.to_string()))?;
+    
+        match email_regex.is_match(value) {
+            true => Ok(Self(value.to_string())),
+            false => Err(DomainError::InvalidEmail(value.to_string()))
         }
-        else {
-            Err(DomainError::InvalidEmail(value.to_string()))
-        }
+
     }
     pub fn value(&self) -> &str {
         &self.0
