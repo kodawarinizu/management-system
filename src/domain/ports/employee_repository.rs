789b13b6eee37760a127fc5be860093d25cdfd13
kodawarinizu <tomas.serey.gonzalez@protonmail.com>
2026-03-30 21:28:50 -0300

    feat (EmployeeRepository): Add find_all trait

--- src/domain/ports/employee_repository.rs
+++ src/domain/ports/employee_repository.rs
@@ -6,6 +6,7 @@ use crate::domain::errors::DomainError;
 #[async_trait]
 pub trait EmployeeRepository {
     async fn save(&self, employee: Employee) -> Result<(), DomainError>;
+    async fn find_all(&self) -> Result<Vec<Employee>, DomainError>;
     async fn find_by_id(&self, id: Uuid) -> Result<Option<Employee>, DomainError>;
     async fn find_by_email(&self, email: &str) -> Result<Option<Employee>, DomainError>;
     async fn update(&self, employee: Employee) -> Result<(), DomainError>;
