853efd308cc9574313709240aeeb0f3b68e9c20a
kodawarinizu <tomas.serey.gonzalez@protonmail.com>
2026-04-02 19:50:37 -0300

    refactor: add employee parameter borrow

--- src/domain/ports/employee_repository.rs
+++ src/domain/ports/employee_repository.rs
@@ -5,10 +5,10 @@ use crate::domain::errors::DomainError;
 
 #[async_trait]
 pub trait EmployeeRepository {
-    async fn save(&self, employee: Employee) -> Result<(), DomainError>;
+    async fn save(&self, employee: &Employee) -> Result<(), DomainError>;
     async fn find_all(&self) -> Result<Vec<Employee>, DomainError>;
     async fn find_by_id(&self, id: Uuid) -> Result<Option<Employee>, DomainError>;
     async fn find_by_email(&self, email: &str) -> Result<Option<Employee>, DomainError>;
-    async fn update(&self, employee: Employee) -> Result<(), DomainError>;
+    async fn update(&self, employee: &Employee) -> Result<(), DomainError>;
     async fn delete(&self , id: Uuid) -> Result<(), DomainError>;
 }
