08ebc3e439993df2a17f0e47b9223aee9e92b7b4
kodawarinizu <tomas.serey.gonzalez@protonmail.com>
2026-04-02 19:48:37 -0300

    feat: add create_employee use case

--- src/application/employee/create_employee.rs
+++ src/application/employee/create_employee.rs
@@ -1,10 +1,7 @@
 use std::sync::Arc;
-use crate::domain::{
-    entities::employee::{Employee, Departament},
-    value_objects::{email::Email, password_hash::HashedPassword},
-    ports::employee_repository::EmployeeRepository,
-    errors::DomainError,
-};
+use crate::{application::employee::{self, create_employee}, domain::{
+    entities::employee::{Departament, Employee}, errors::DomainError, ports::employee_repository::EmployeeRepository, value_objects::{email::Email, password_hash::HashedPassword}
+}};
 
 pub struct CreateEmployeeInput {
     pub name:  String,
@@ -16,4 +13,30 @@ pub struct CreateEmployeeInput {
 
 pub struct CreateEmployeeUseCase {
     repository: Arc<dyn EmployeeRepository>,
+}
+
+impl CreateEmployeeUseCase {
+    pub fn new (repo: Arc<dyn EmployeeRepository>) -> Self {
+        Self { repository: repo }
+    }
+    async fn execute (&self, input: CreateEmployeeInput) -> Result<Employee, DomainError> {
+        let email = Email::new(&input.email)?;
+        if self.repository.find_by_email(&email.value()).await?.is_some() {
+            return Err(DomainError::InvalidEmail(email.value().to_string()));
+        }
+
+        let passhash = HashedPassword::new(&input.password_hash)?;
+
+        let employee = Employee::new(
+            input.name, 
+            input.departament, 
+            email,
+            passhash, 
+            input.salary
+        );
+
+        self.repository.save(&employee).await?;
+
+        Ok(employee)
+    }
 }
