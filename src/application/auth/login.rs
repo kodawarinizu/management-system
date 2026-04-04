19d96d497eb01628b018575bb0e5116d465e0096
kodawarinizu <tomas.serey.gonzalez@protonmail.com>
2026-04-04 00:14:39 -0300

    feat: add login & register UseCase

---
+++ src/application/auth/login.rs
@@ -0,0 +1,28 @@
+use std::sync::Arc;
+
+use crate::domain::{
+    entities::employee::Employee, 
+    errors::DomainError, 
+    ports::employee_repository::EmployeeRepository, value_objects::password_hash::HashedPassword
+};
+
+pub struct LoginUseCase {
+    repository: Arc<dyn EmployeeRepository>
+}
+
+impl LoginUseCase {
+    pub fn new (repo: Arc<dyn EmployeeRepository>) -> Self {
+        Self { repository: repo }
+    }
+
+    pub async fn execute (&self, email: &str, password: &str) -> Result<Employee, DomainError> {
+        let employee = self.repository.find_by_email(email)
+            .await?
+            .ok_or(DomainError::InvalidCredentials("Email or Password incorrect".to_string()))?;
+
+        match employee.password_hash.verify(password)? {
+            true => Ok(employee),
+            false => Err(DomainError::InvalidCredentials("Email or Password incorrect".to_string())),
+        }
+    }
+}
