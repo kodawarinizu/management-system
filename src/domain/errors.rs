efbfd90e6dec89d32dc5e10dcc12dda3086d91f8
kodawarinizu <tomas.serey.gonzalez@protonmail.com>
2026-04-04 00:11:02 -0300

    feat(errors): add InvalidCredentials custom error

--- src/domain/errors.rs
+++ src/domain/errors.rs
@@ -2,20 +2,30 @@ use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum DomainError {
-    #[error("[Employee Error]: {0}")]
+    #[error("[Invalid Credentials!]: {0}")]
+    InvalidCredentials(String),
+
+    #[error("[Employee Error!]: {0}")]
     EmployeeError(String),
-    #[error("[Invalid Salary]: {0}")]
+
+    #[error("[Invalid Salary!]: {0}")]
     InvalidSalary(String),
+
     #[error("[Invalid Email!]: {0}")]
     InvalidEmail(String),
+
     #[error("[PasswordHash Error!]: {0}")]
     HashError(String),
+
     #[error("[DataBase Error!]: {0}")]
     DatabaseError(String),
+
     #[error("[Departament Error]: {0}")]
     DepartamentError(String),
+
     #[error("[Duplicate Email!]")]
     DuplicateEmail,
+
     #[error("[Employee Not Found!]: {0}")]
     EmployeeNotFound(String),
 }
